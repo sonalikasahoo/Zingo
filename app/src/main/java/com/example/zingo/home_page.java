33e14cc87595e539b0cab0f5a93b6b1ecd0389c4
Anjalikumari15 <arukia97@gmail.com>
2019-02-28 01:59:46 +0530

    modified manifest and settings menu

--- app/src/main/java/com/example/zingo/home_page.java
+++ app/src/main/java/com/example/zingo/home_page.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,12 +31,12 @@ public class home_page extends AppCompatActivity {
         Intent receivedIntent = getIntent();
         final String phNumber = receivedIntent.getStringExtra("phNumber");
 
-        final TextView name,licence,phone,email,region,address;
-        name = (TextView)findViewById(R.id.wh_name);
-        licence = (TextView)findViewById(R.id.license_no);
-        phone = (TextView)findViewById(R.id.wh_number);
-        email = (TextView)findViewById(R.id.wh_mail);
-        address = (TextView)findViewById(R.id.address);
+        final TextView name, licence, phone, email, region, address;
+        name = (TextView) findViewById(R.id.wh_name);
+        licence = (TextView) findViewById(R.id.license_no);
+        phone = (TextView) findViewById(R.id.wh_number);
+        email = (TextView) findViewById(R.id.wh_mail);
+        address = (TextView) findViewById(R.id.address);
 
 
         final DatabaseReference databaseReference = FirebaseDatabase.getInstance()
@@ -47,8 +49,8 @@ public class home_page extends AppCompatActivity {
                 phone.setText(phNumber);
                 email.setText(dataSnapshot.child("email").getValue().toString());
                 address.setText(dataSnapshot.child("address1").getValue().toString()
-                +", "+dataSnapshot.child("city").getValue().toString() + ", "
-                +dataSnapshot.child("state").getValue().toString() + ", "+
+                        + ", " + dataSnapshot.child("city").getValue().toString() + ", "
+                        + dataSnapshot.child("state").getValue().toString() + ", " +
                         dataSnapshot.child("pincode").getValue().toString());
             }
 
@@ -58,44 +60,57 @@ public class home_page extends AppCompatActivity {
             }
         });
 
+        TextView return1, date1, time1, product1, status1;
+        TextView return2, date2, time2, product2, status2;
+        TextView return3, date3, time3, product3, status3;
 
+        return1 = (TextView) findViewById(R.id.return_1);
+        return2 = (TextView) findViewById(R.id.return_2);
+        return3 = (TextView) findViewById(R.id.return_3);
 
+        date1 = (TextView) findViewById(R.id.date_1);
+        date2 = (TextView) findViewById(R.id.date_2);
+        date3 = (TextView) findViewById(R.id.date_3);
 
+        time1 = (TextView) findViewById(R.id.time_1);
+        time2 = (TextView) findViewById(R.id.time_2);
+        time3 = (TextView) findViewById(R.id.time_3);
 
-        TextView return1,date1,time1,product1,status1;
-        TextView return2,date2,time2,product2,status2;
-        TextView return3,date3,time3,product3,status3;
+        product1 = (TextView) findViewById(R.id.product);
+        product2 = (TextView) findViewById(R.id.product2);
+        product3 = (TextView) findViewById(R.id.product3);
 
-        return1 = (TextView)findViewById(R.id.return_1);
-        return2 = (TextView)findViewById(R.id.return_2);
-        return3 = (TextView)findViewById(R.id.return_3);
+        status1 = (TextView) findViewById(R.id.state);
+        status2 = (TextView) findViewById(R.id.stat2);
+        status3 = (TextView) findViewById(R.id.stats3);
 
-        date1 = (TextView)findViewById(R.id.date_1);
-        date2 = (TextView)findViewById(R.id.date_2);
-        date3 = (TextView)findViewById(R.id.date_3);
-
-        time1 = (TextView)findViewById(R.id.time_1);
-        time2 = (TextView)findViewById(R.id.time_2);
-        time3 = (TextView)findViewById(R.id.time_3);
-
-        product1 = (TextView)findViewById(R.id.product);
-        product2 = (TextView)findViewById(R.id.product2);
-        product3 = (TextView)findViewById(R.id.product3);
-
-        status1 = (TextView)findViewById(R.id.state);
-        status2 = (TextView)findViewById(R.id.stat2);
-        status3 = (TextView)findViewById(R.id.stats3);
-
-        Button addRetailer = (Button)findViewById(R.id.retailer_add);
+        Button addRetailer = (Button) findViewById(R.id.retailer_add);
 
         addRetailer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent home_page;
-               home_page = new Intent(getApplicationContext(),add_retailer.class);
-                  startActivity(home_page);
+                home_page = new Intent(getApplicationContext(), add_retailer.class);
+                startActivity(home_page);
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_from_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, add_retailer.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
